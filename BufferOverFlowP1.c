86c107941feba2795efdd376fe3a6086478661d8
Akira <83699147+akiraDUH@users.noreply.github.com>
2024-07-23 16:34:29 +0300

    Completed_P1_EX4

---
+++ BufferOverFlowP1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    char password[16];
+    int flag=0;
+    printf("ENter password\n");
+    scanf("%s",password);
+    if(strcmp(password,"123")==0){
+        printf("Welcome!");
+        flag=1;
+   }
+   else{
+    printf("WRong password\n");
+   }
+   if(flag){
+    printf("ROOOOOOOOOOOOOT ACCESSSSSSSSSSSSSSSSS\n");
+   }
+   return 0;
+}
